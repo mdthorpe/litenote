7a76713bdaf7b58812f78d52f4b58a55ba46c5ab
Mike Thorpe <mt@foo.ca>
2011-07-20 13:22:20 -0700

    Updated reporting

--- scripts/list_poll_events.py
+++ scripts/list_poll_events.py
@@ -7,7 +7,7 @@ from pymongo import Connection
 connection = pymongo.Connection('localhost')
 db = connection['litenote']
 for base_url in  db.poller_base_url.find():
-    print "[%s] '%s' '%s'" % (time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime(base_url['poll_start_time'])), base_url['base_url'], base_url['_id'])
+    print "[%s] '%s' '%s'" % (time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(base_url['poll_start_time'])), base_url['base_url'], base_url['_id'])
     #for resource in  db.poller_resource.find({"base_url_poll_id": id}):
     #    print "\t[%d] [%0.2fs] %s" % (resource['http_code'], resource['request_time'], resource['full_url'])
 
